1450593086fca771fbde5c26462ba1f873154b8a
Stephen Brough <StephenR.Brough@gmail.com>
2013-11-03 20:34:57 -0700

    Added other templates to templateController.js

--- js/templateController.js
+++ js/templateController.js
@@ -1,5 +1,10 @@
-function templateController($scope)
+function templateController($scope) {
 	$scope.templates = 
-		[ { name: 'diary.html', url: 'diary.html'}
-		, { name: 'detail.html', url: 'detail.html'} ];
+		[ { name: 'diary.html', url: 'templates/diary.html'}
+		, { name: 'detail.html', url: 'templates/detail.html'}
+		, { name: 'add.html', url: 'templates/add.html'}
+		, { name: 'nutrition.html', url: 'templates/nutrition.html'}
+		, { name: 'recent.html', url: 'templates/recent.html'}
+		, { name: 'scan.html', url: 'templates/scan.html'} ];
 	$scope.template = $scope.templates[0];
+}
